2edfb3ec0d40ce342c0be034d0b87b9045d06c9b
Hawila <abdulrahman.hawila@gmail.com>
2023-03-16 12:21:49 +0200

    add ec2 terraform

--- terraform/ec2/ec2.tf
+++ terraform/ec2/ec2.tf
@@ -20,7 +20,13 @@ resource "aws_default_subnet" "default_az1" {
 
 resource "aws_instance" "ec2_instance" {
     ami = "ami-0557a15b87f6559cf"
-    subnet_id = "${aws_subnet.default_az1.id}"
+    subnet_id = "${aws_default_subnet.default_az1.id}"
     instance_type = "t2.micro"
-    vpc_security_group_ids = ["sg-1"]
+    key_name = "key"
+    vpc_security_group_ids = [aws_security_group.sg-1.id]
+    root_block_device {
+    volume_type = "gp2"
+    volume_size = "8"
+    delete_on_termination = true
+    }
 } 
--- terraform/ec2/sg.tf
+++ terraform/ec2/sg.tf
@@ -1,14 +1,29 @@
 resource "aws_security_group" "sg-1" {
-  name        = "sg-1"
-  description = "Allow All traffic"
-  vpc_id      = aws_vpc.default.id
+  name        = "ec2 sg"
+   description = "Allow HTTP and SSH traffic via Terraform"
+  vpc_id      = aws_default_vpc.default.id
+
+   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
-    protocol  = -1
-    self      = true
-    from_port = 0
-    to_port   = 0
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+  
+
   egress {
     from_port        = 0
     to_port          = 0
